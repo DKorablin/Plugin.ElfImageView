8c1a335248e25165c6fd2998402a4492341ceaca
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 03:43:05 +0300

    Unify plugins deployment

--- Plugin.ElfImageView/Properties/AssemblyInfo.cs
+++ Plugin.ElfImageView/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("1c34fd01-39a7-4774-b330-60d33b3c1b0d")]
 [assembly: System.CLSCompliant(false)]
 
@@ -9,15 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=112")]
 #else
 
-[assembly: AssemblyTitle("Plugin.ElfImageView")]
 [assembly: AssemblyDescription("Read Executable and Linkable Format image viewer")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.ElfImageView")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2024")]
 #endif
 
